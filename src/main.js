d403f05d42defbbd09b3749cbadcc1d622aed057
Jonas <jonathan123.martinez@gmail.com>
2023-05-23 15:24:51 -0400

    adding analytics

--- src/main.js
+++ src/main.js
@@ -14,7 +14,9 @@ import '../styles/utils.css';
 import mobileNav from './utils/mobile-nav';
 import darkMode from './utils/dark-mode';
 import lazyLoading from './utils/lazy-loading';
+import { inject } from '@vercel/analytics';
 
 mobileNav();
 darkMode();
 lazyLoading();
+inject();
